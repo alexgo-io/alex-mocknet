730a91132fa6eb2b170f20310319386d1a2530d3
fiftyeightandeight <alexd@alexgo.io>
2021-12-03 15:04:39 +0800

    Update set-up-mocknet.sh

--- set-up-mocknet.sh
+++ set-up-mocknet.sh
@@ -2,4 +2,4 @@
 
 docker pull blockstack/stacks-blockchain-api && docker pull blockstack/stacks-blockchain && docker pull postgres:alpine
 docker network create stacks-blockchain > /dev/null 2>&1
-mkdir -p ./{persistent-data/postgres,persistent-data/stacks-blockchain,bns,config}
+# mkdir -p ./{persistent-data/postgres,persistent-data/stacks-blockchain,bns,config}
