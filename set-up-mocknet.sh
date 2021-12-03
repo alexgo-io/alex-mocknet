6f62afa03be33f24a513b87d9b37172252371826
fiftyeightandeight <alexd@alexgo.io>
2021-12-03 13:07:13 +0800

    Create set-up-mocknet.sh

---
+++ set-up-mocknet.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+docker pull blockstack/stacks-blockchain-api && docker pull blockstack/stacks-blockchain && docker pull postgres:alpine
+docker network create stacks-blockchain > /dev/null 2>&1
+mkdir -p ./{persistent-data/postgres,persistent-data/stacks-blockchain,bns,config}
