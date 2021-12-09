0d8d8e7e9009b48e4104f86fadade0f918f47472
Zitao Xiong <caoer115@gmail.com>
2021-12-09 16:47:50 +0800

    chore: cleanup script format for better readability

--- set-up-mocknet.sh
+++ set-up-mocknet.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-docker pull blockstack/stacks-blockchain-api && docker pull blockstack/stacks-blockchain && docker pull postgres:alpine
+docker pull blockstack/stacks-blockchain-api \
+  && docker pull blockstack/stacks-blockchain \
+  && docker pull postgres:alpine \
+  ;
+
 docker network create stacks-blockchain > /dev/null 2>&1
 # mkdir -p ./{persistent-data/postgres,persistent-data/stacks-blockchain,bns,config}
